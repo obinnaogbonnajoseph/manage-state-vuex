deeb50e2ee2e8663364c3c07d82b92a0e51b32e5
Obinna Ogbonna <oogbonna@gidimobile.com>
2020-10-26 05:33:27 +0100

    add fetching data from api add axios package add a config file for local development to use api

--- src/store/index.js
+++ src/store/index.js
@@ -2,17 +2,29 @@
 /* eslint-disable eol-last */
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
         cart: [],
+        parts: null
     },
     mutations: {
         addRobotToCart(state, robot) {
             state.cart.push(robot);
         },
+        updateParts(state, parts) {
+            state.parts = parts;
+        }
+    },
+    actions: {
+        getParts({ commit }) {
+            axios.get('/api/parts')
+                .then(result => commit('updateParts', result.data))
+                .catch(console.error);
+        }
     },
     getters: {
         cartSaleItems(state) {
