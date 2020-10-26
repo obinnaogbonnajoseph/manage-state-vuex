8e01bb34c5ba4e7e0cab6d02d973f9e7d6120e10
Obinna Ogbonna <oogbonna@gidimobile.com>
2020-10-26 05:02:14 +0100

    add cart link on nav items remove old implementation of displaying cart

--- src/App.vue
+++ src/App.vue
@@ -4,34 +4,44 @@
       <nav>
         <ul>
           <li class="nav-item">
-            <router-link class="nav-link" :to="{name: 'Home'}" exact>
-              <img class="logo" src="./assets/build-a-bot-logo.png"/>
+            <router-link class="nav-link" :to="{ name: 'Home' }" exact>
+              <img class="logo" src="./assets/build-a-bot-logo.png" />
               Build-a-Bot
             </router-link>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" :to="{name: 'Build'}" exact>
+            <router-link class="nav-link" :to="{ name: 'Build' }" exact>
               Build
             </router-link>
+          </li>
+          <li class="nav-item cart">
+            <router-link class="nav-link" to="/cart" exact> Cart </router-link>
+            <div class="cart-items">
+              {{ cart.length }}
+            </div>
           </li>
         </ul>
       </nav>
     </header>
     <div class="container">
       <aside class="aside">
-        <router-view name="sidebar"/>
+        <router-view name="sidebar" />
       </aside>
       <main>
-        <router-view/>
+        <router-view />
       </main>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'app',
+  name: "app",
+  computed: {
+    cart() {
+      return this.$store.state.cart;
+    },
+  },
 };
 </script>
 
@@ -44,7 +54,7 @@ body {
 
 <style scoped>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
 }
 main {
   padding: 30px;
@@ -67,6 +77,11 @@ ul {
   font-size: 22px;
   border-right: 1px solid #bbb;
 }
+.nav-item.cart {
+  position: relative;
+  margin-left: auto;
+  border-right: none;
+}
 .logo {
   vertical-align: middle;
   height: 30px;
@@ -88,5 +103,16 @@ ul {
   background-color: #aaa;
   width: 100px;
   min-height: 300px;
+}
+.cart-items {
+  position: absolute;
+  top: -5px;
+  right: -9px;
+  font-size: 18px;
+  width: 20px;
+  text-align: center;
+  display: inline-block;
+  border-radius: 100px;
+  background-color: mediumseagreen;
 }
 </style>
